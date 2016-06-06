5ababb189a910bab3f0eb682de3fc7356aea685f
Charles Drolet <cdrolet@expedia.com>
2016-06-06 15:42:35 -0400

    adding rest api

---
+++ src/main/java/org/cdrokar/saboteur/web/SaboteurController.java
@@ -0,0 +1,28 @@
+package org.cdrokar.saboteur.web;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.Collection;
+
+import javax.inject.Inject;
+
+import org.cdrokar.saboteur.SaboteurRepository;
+import org.cdrokar.saboteur.disruption.Disruptive;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/saboteur", produces = "application/json")
+@RequiredArgsConstructor(onConstructor = @__(@Inject))
+public class SaboteurController {
+
+    private final SaboteurRepository repository;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/disruptives")
+    Collection<Disruptive> getDisruptives() {
+        return Disruptive.REGISTRY;
+    }
+
+    
+}
